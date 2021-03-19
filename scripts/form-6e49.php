0547f757bf5de3cf8bad7290da66267e3a89192f
Brady Jenkins <crushedpvppe@gmail.com>
2021-03-18 22:36:35 -0400

    Add files via upload

---
+++ scripts/form-6e49.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once('FormProcessor.php');
+
+$form = array(
+    'subject' => 'New Site Contact - Ellie&#39;s Art',
+    'email_message' => 'New Message',
+    'success_redirect' => '',
+    'sendIpAddress' => true,
+    'email' => array(
+    'from' => 'contact@elliesart.ml',
+    'to' => 'arkus_e@nrevsd.com'
+    ),
+    'fields' => array(
+    'name' => array(
+    'order' => 1,
+    'type' => 'string',
+    'label' => 'Name',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Name\' is required.'
+    )
+    ),
+    'email' => array(
+    'order' => 2,
+    'type' => 'email',
+    'label' => 'Email',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Email\' is required.'
+    )
+    ),
+    'message' => array(
+    'order' => 3,
+    'type' => 'string',
+    'label' => 'Message',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Message\' is required.'
+    )
+    ),
+    )
+    );
+
+    $processor = new FormProcessor();
+    $processor->process($form);
+
+    ?>
